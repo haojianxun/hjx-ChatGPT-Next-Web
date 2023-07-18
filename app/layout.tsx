842cd4b039fd6122eeff20173ded4e44edacafad
Joe <me@qjidea.com>
2023-07-18 09:28:03 +0800

    feat: ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,18 +34,16 @@ export default function RootLayout({
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2935642389749184"
-     crossorigin="anonymous"></script>
-      <!-- chat -->
-      <ins class="adsbygoogle"
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2935642389749184" crossorigin="anonymous"></script>
+        <ins class="adsbygoogle"
           style="display:block"
           data-ad-client="ca-pub-2935642389749184"
           data-ad-slot="7953564210"
           data-ad-format="auto"
           data-full-width-responsive="true"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
       </head>
       <body>{children}</body>
     </html>
