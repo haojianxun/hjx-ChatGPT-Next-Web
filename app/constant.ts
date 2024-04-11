3c546fea5960082cda5a7bbbf9b8332dc394b79b
A-Cepheus <60658915+A-Cepheus@users.noreply.github.com>
2024-04-12 00:49:58 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -126,7 +126,8 @@ export const SUMMARIZE_MODEL = "gpt-3.5-turbo";
 export const GEMINI_SUMMARIZE_MODEL = "gemini-pro";
 
 export const KnowledgeCutOffDate: Record<string, string> = {
-  default: "2021-09",
+  default: "2023-06",
+  "gpt-4-turbo": "2023-12",
   "gpt-4-turbo-preview": "2023-12",
   "gpt-4-1106-preview": "2023-04",
   "gpt-4-0125-preview": "2023-12",
@@ -134,9 +135,6 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   // After improvements,
   // it's now easier to add "KnowledgeCutOffDate" instead of stupid hardcoding it, as was done previously.
   "gemini-pro": "2023-12",
-  "Yi-34B-Chat": "2023-06",
-  "Yi-34B-Chat-4bits": "2023-06",
-  "Yi-34B-Chat-8bits": "2023-06",
 };
 
 export const DEFAULT_MODELS = [
